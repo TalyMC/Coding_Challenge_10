6922414c0a19ab9c50c8a1466065b01e28680a82
Natalie Mauricio-Cruz <mauriciocruzn@usf.edu>
2025-02-23 23:30:38 -0500

    Task 5 - Implemented Product Restocking

--- cc_10.js
+++ cc_10.js
@@ -57,7 +57,6 @@ class Inventory {
     listProducts(){
         this.products.forEach(product => console.log(product.getDetails()));
     };
-
     // //Task 4 Implementing Order Management
     placeOrder(orderId, product, quantity) {
         if (product.stock >= quantity) {
@@ -69,6 +68,11 @@ class Inventory {
             console.log(order.getOrderDetails());
         });
     };
+    //Task 5 Implemented Product Restocking
+    restockProduct(productId, quantity) {
+        const product = this.products.find((prod) => prod.id === productId); // finds product
+        product.stock += quantity;
+    };
 };
 
 const inventory = new Inventory();
@@ -77,9 +81,15 @@ inventory.listProducts();
 // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5"
 
 
-//Task 4
+//Task 4 Implementing Order Management
 inventory.placeOrder(601, prod1, 2);
 inventory.listOrders();
 // Expected output: "Order ID: 601, Product: Laptop, Quantity: 2, Total Price: $2400"
 console.log(prod1.getDetails());
 // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 3"
+
+
+//Task 5 Implementing Product Restocking
+inventory.restockProduct(101, 5);
+console.log(prod1.getDetails()); 
+// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 8"
