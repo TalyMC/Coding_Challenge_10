72776e62587e64811af3f353002214477be03976
Natalie Mauricio-Cruz <mauriciocruzn@usf.edu>
2025-02-23 23:19:36 -0500

    Task 3 - Created Inventory Class

--- cc_10.js
+++ cc_10.js
@@ -42,3 +42,22 @@ console.log(order1.getOrderDetails());
 
 console.log(prod1.getDetails());
 // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5"
+
+
+//Task 3 Creating an Inventory Class
+class Inventory {
+    constructor(products) {
+        this.products = [];
+    };
+    addProduct(product) {
+        this.products.push(product);
+    };
+    listProducts(){
+        this.products.forEach(product => console.log(product.getDetails()));
+    };
+};
+
+const inventory = new Inventory();
+inventory.addProduct(prod1);
+inventory.listProducts();
+// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5"
